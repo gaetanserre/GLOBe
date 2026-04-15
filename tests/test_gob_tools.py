adcb3a876c0f5a0fb9f49e10f34c986a51812f2f
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2026-04-16 00:03:53 +0200

    test_optimizers.py

--- tests/test_gob_tools.py
+++ tests/test_gob_tools.py
@@ -1,37 +1,21 @@
-#
-# Created in 2024 by Gaëtan Serré
-#
-
-from gob import GOB
-from gob.benchmarks import PyGKLS, create_bounds, augment_dimensions
+from gob.benchmarks import *
+import gob.optimizers as go
 import inspect
-import gob.benchmarks as gb
 
-n_particles = 15
-iter = 10
+
+pygkls = PyGKLS(2, 15, [-100, 100], -100, smoothness="ND", gen=42)
+
+f = Square()
+
+bounds = augment_dimensions(f.visual_bounds, 2)  # f.visual_bounds
+
+n_particles = 150
+iter = 1000
 sigma = 1 / n_particles**2
-dim = 2
+verbose = False
 
-if __name__ == "__main__":
-    pygkls = PyGKLS(dim, 15, [-100, 100], -100, smoothness="ND")
-
-    benchmarks = []
-    bounds = []
-    for name, obj in inspect.getmembers(gb, inspect.isclass):
-        if name != "PyGKLS":
-            benchmarks.append(obj())
-            bounds.append(augment_dimensions(benchmarks[-1].visual_bounds, dim))
-    benchmarks.append(pygkls)
-    bounds.append(create_bounds(dim, -99, 99))
-
-    gob = GOB(
-        [
-            ("Langevin", {"n_particles": n_particles, "iter": iter}),
-            ("SBS", {"n_particles": n_particles, "iter": iter, "sigma": sigma}),
-            ("CBO", {"n_particles": n_particles, "iter": iter}),
-        ],
-        benchmarks[:3],
-        [],
-        bounds=bounds,
-    )
-    gob.run(n_runs=5, verbose=1, latex_table=True)
+optimizers = inspect.getmembers(go, inspect.isclass)
+for _, opt in optimizers:
+    opt = opt(bounds=bounds)
+    res = opt.minimize(f)
+    print(f"Results for {opt} on {f}: {res[1]}")
